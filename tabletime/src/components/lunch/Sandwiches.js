ef434da4366a5709e5922fecd2f097602496be3d
Daniel Park <dpeace926@gmail.com>
2017-10-15 23:36:02 -0400

    create components and container

---
+++ tabletime/src/components/lunch/Sandwiches.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sandwiches () {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Sandwiches;
---
+++ tabletime/src/containers/MenuContainer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class MenuContainer extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+
+    }
+  }
+  
+  render = () => {
+
+  }
+}
+
+export default MenuContainer
