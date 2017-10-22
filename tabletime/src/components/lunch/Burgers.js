c91a4f0e29910b0708e06d758a40c1c1ef2c5649
Daniel Park <dpeace926@gmail.com>
2017-10-22 00:43:33 -0400

    add lunch contents

--- tabletime/src/components/lunch/Burgers.js
+++ tabletime/src/components/lunch/Burgers.js
@@ -4,6 +4,35 @@ function Burgers () {
   return (
     <div>
       [burger]
+      <h1>BURGERS</h1>
+      <h5>(ALL NATURAL 1/2 LB BEEF)</h5>
+      <ul>
+      HAMBURGER
+      $4.50
+      CHEESEBURGER
+      $4.95
+      BACON CHEESE BURGER
+      $5.95
+      TURKEY BURGER
+      $4.95
+      VEGGIE BURGER
+      $4.95
+      CALIFORNIA BURGER
+      $6.95
+      BACON AVOCADO, ALFALFA SPROUTS, LETTUCE, TOMATOES
+      </ul>
+
+      ADDITIONALS
+      <ul>
+      DELUXE BURGER SERVED WITH FRENCH FRIES, LETTUCE, TOMATOES, PICKLE
+      $2.00 ADDITIONAL
+      ADD TOMATOES
+      $0.50
+      ADD CHEESE
+      $0.50
+      ADD BACON
+      $1.00
+      </ul>
     </div>
   )
 }
--- tabletime/src/components/lunch/Quesadillas.js
+++ tabletime/src/components/lunch/Quesadillas.js
@@ -4,6 +4,16 @@ function Quesadillas () {
   return (
     <div>
       [quesadilla]
+      <h1>QUESADILLA</h1>
+      CHEESE
+      MONTEREY JACK & CHEDDAR CHEESE, PEPPERS, ONIONS, SLICED CHILIES
+      $6.95
+      GRILLED CHICKEN
+      MONTEREY JACK & CHEDDAR CHEESE, PEPPERS, ONIONS, SLICED CHILIES
+      $7.95
+      GRILLED STEAK
+      MONTEREY JACK & CHEDDAR CHEESE, PEPPERS, ONIONS, SLICED CHILIES
+      $7.95
     </div>
   )
 }
