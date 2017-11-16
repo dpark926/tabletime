656587e316054f151ecdfd585d8e7d9b1ab9e3e7
Daniel Park <dpeace926@gmail.com>
2017-11-15 23:35:26 -0500

    build Salad component

--- tabletime/src/components/lunch/Sandwiches.js
+++ tabletime/src/components/lunch/Sandwiches.js
@@ -12,26 +12,18 @@ function Sandwiches () {
           <span>ITALIAN<br/>
           GENOA SALAMI, CAPICOLA, PROVOLONE CHEESE, LETTUCE, TOMATOES, ONIONS, HOT PEPPERS, OIL & VINEGAR</span>
         </div>
-      </div>
-      <div>
         <div className='item-box'>
           <span>AMERICAN<br/>
           VIRGINIA HAM, BLACK FOREST HAM, AMERICAN CHEESE, LETTUCE, TOMATOES, MAYO</span>
         </div>
-      </div>
-      <div>
         <div className='item-box'>
           <span>TABLETIME<br/>
           SMOKED TURKEY, CORNED BEEF, COLESLAW, SWISS CHEESE, RUSSIAN DRESSING</span>
         </div>
-      </div>
-      <div>
         <div className='item-box'>
           <span>CT<br/>
           TURKEY BREAST, CORNED BEEF, COLESLAW, SWISS CHEESE, RUSSIAN DRESSING</span>
         </div>
-      </div>
-      <div>
         <div className='item-box'>
           <span>GREEN TURKEY<br/>
           TURKEY BREAST, BRIE CHEESE, SLICED GRANNY SMITH APPLE, LETTUCE, HONEY MUSTARD</span>
@@ -40,67 +32,73 @@ function Sandwiches () {
 
       <h1>VEGETARIAN SANDWICHES</h1>
       <h5>SLICED BREAD or ROLL $7.49 / WRAPS or HERO $8.99</h5>
-      <div>
-        MOZZARELLA DELIGHT
-        FRESH MOZZARELLA CHEESE ROASTED RED PEPPERS, TOMATO, BASIL, ITALIAN DRESSING
-      </div>
-      <div>
-        LITE DELIGHTS
-        GRILLED VEGETABLES, GREEN SQUASH, YELLOW SQUASH, EGGPLANT, CARROTS, FRESH MOZZARELLA CHEESE, ROASTED RED PEPPERS, HUMMUS TAHINI SAUCE
-      </div>
-      <div>
-        VEGETARIAN SPECIAL
-        AVOCADO, ROMAINE LETTUCE, TOMATOES, CARROTS, CUCUMBERS, ALFAFA SPROUTS, SHREDDED RED CABBAGE, SWEET PEPPERS, HONEY MUSTARD
-      </div>
-      <div>
-        EGGPLANT & ZUCCHINI
-        EGGPLANTS & ZUCCHINI MARINATED WITH BALSAMIC VINEGAR, ROASTED RED PEPPERS, SMOKED MOZZARELLA CHEESE, HONEY DIJON MUSTARD
+      <div className='menu-wrapper'>
+        <div className='item-box'>
+          MOZZARELLA DELIGHT<br/>
+          FRESH MOZZARELLA CHEESE ROASTED RED PEPPERS, TOMATO, BASIL, ITALIAN DRESSING
+        </div>
+        <div className='item-box'>
+          LITE DELIGHTS<br/>
+          GRILLED VEGETABLES, GREEN SQUASH, YELLOW SQUASH, EGGPLANT, CARROTS, FRESH MOZZARELLA CHEESE, ROASTED RED PEPPERS, HUMMUS TAHINI SAUCE
+        </div>
+        <div className='item-box'>
+          VEGETARIAN SPECIAL<br/>
+          AVOCADO, ROMAINE LETTUCE, TOMATOES, CARROTS, CUCUMBERS, ALFAFA SPROUTS, SHREDDED RED CABBAGE, SWEET PEPPERS, HONEY MUSTARD
+        </div>
+        <div className='item-box'>
+          EGGPLANT & ZUCCHINI<br/>
+          EGGPLANTS & ZUCCHINI MARINATED WITH BALSAMIC VINEGAR, ROASTED RED PEPPERS, SMOKED MOZZARELLA CHEESE, HONEY DIJON MUSTARD
+        </div>
       </div>
 
       <h1>SIGNATURE HOT SANDWICHES</h1>
       <h5>SLICED BREAD or ROLL $7.49 / WRAPS or HERO $8.99</h5>
-      <div>
-        "T" COMBO
-        HOT PASTRAMI, CORNED BEEF, SWISS CHEESE, COLE SLAW, RUSSIAN DRESSING
-      </div>
-      <div>
-        THE PHILLY
-        GRILLED PHILLY STEAK, MELTED AMERICAN CHEESE, GRILLED ONIONS AND PEPPERS
-      </div>
-      <div>
-        THE SMOKY
-        SMOKED HAM, SMOKED TURKEY, SWISS CHEESE, COLESLAW, RUSSIAN DRESSING
-      </div>
-      <div>
-        THE MAXIMUS
-        GRILLED SMOKED TURKEY, MELTED PEPPER JACK CHEESE, CRISPY BACON, SUNDRIED TOMATO PASTE
-      </div>
-      <div>
-        THE AMAZING
-        OVEN ROASTED FRESH TURKEY, MELTED SWISS CHEESE, ROASTED PEPPERS, CRANBERRY SAUCE
+      <div className='menu-wrapper'>
+        <div className='item-box'>
+          "T" COMBO<br/>
+          HOT PASTRAMI, CORNED BEEF, SWISS CHEESE, COLE SLAW, RUSSIAN DRESSING
+        </div>
+        <div className='item-box'>
+          THE PHILLY<br/>
+          GRILLED PHILLY STEAK, MELTED AMERICAN CHEESE, GRILLED ONIONS AND PEPPERS
+        </div>
+        <div className='item-box'>
+          THE SMOKY<br/>
+          SMOKED HAM, SMOKED TURKEY, SWISS CHEESE, COLESLAW, RUSSIAN DRESSING
+        </div>
+        <div className='item-box'>
+          THE MAXIMUS<br/>
+          GRILLED SMOKED TURKEY, MELTED PEPPER JACK CHEESE, CRISPY BACON, SUNDRIED TOMATO PASTE
+        </div>
+        <div className='item-box'>
+          THE AMAZING<br/>
+          OVEN ROASTED FRESH TURKEY, MELTED SWISS CHEESE, ROASTED PEPPERS, CRANBERRY SAUCE
+        </div>
       </div>
 
       <h1>HOT SANDWICHES</h1>
       <h5>SLICED BREAD or ROLL $7.49 / WRAPS or HERO $8.99</h5>
-      <div>
-        THE CLASSIC
-        GRILLED CHICKEN BREAST, FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, ROASTED RED PEPPERS
-      </div>
-      <div>
-        THE CHICAVO
-        GRILLED CHICKEN BREAST, AVOCADO, ROASTED RED PEPPERS, MESCLUN, RANCH DRESSING
-      </div>
-      <div>
-        THE TOBASCO
-        GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, HOT PEPPERS, TABASCO SAUCE
-      </div>
-      <div>
-        TERIYAKI CHICKEN
-        GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, TOMATO, TERIYAKI SAUCE
-      </div>
-      <div>
-        HONEY BBQ CHICKEN
-        GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE
+      <div className='menu-wrapper'>
+        <div className='item-box'>
+          THE CLASSIC<br/>
+          GRILLED CHICKEN BREAST, FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, ROASTED RED PEPPERS
+        </div>
+        <div className='item-box'>
+          THE CHICAVO<br/>
+          GRILLED CHICKEN BREAST, AVOCADO, ROASTED RED PEPPERS, MESCLUN, RANCH DRESSING
+        </div>
+        <div className='item-box'>
+          THE TOBASCO<br/>
+          GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, HOT PEPPERS, TABASCO SAUCE
+        </div>
+        <div className='item-box'>
+          TERIYAKI CHICKEN<br/>
+          GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE, TOMATO, TERIYAKI SAUCE
+        </div>
+        <div className='item-box'>
+          HONEY BBQ CHICKEN<br/>
+          GRILLED CHICKEN BREAST, MELTED FRESH MOZZARELLA CHEESE, GRILLED ONIONS, ROMAINE LETTUCE
+        </div>
       </div>
     </div>
   )
